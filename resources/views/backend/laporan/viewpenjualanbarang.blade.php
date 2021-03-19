ba0464a9f95236388bfc181248c4925893a426d0
devasatrio <34015427+devsatrio@users.noreply.github.com>
2021-03-19 20:23:20 +0700

    adding dashboard

--- resources/views/backend/laporan/viewpenjualanbarang.blade.php
+++ resources/views/backend/laporan/viewpenjualanbarang.blade.php
@@ -13,7 +13,7 @@
         <div class="container">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1 class="m-0 text-dark"> Laporan Transaksi</h1>
+                    <h1 class="m-0 text-dark"> Laporan Per barang</h1>
                 </div>
             </div>
         </div>
@@ -24,7 +24,7 @@
                 <div class="col-lg-12">
                     <div class="card card-dark">
                         <div class="card-header">
-                            <h3 class="card-title">Laporan Laporan Transaksi {{$_GET['finaltgl']}}</h3>
+                            <h3 class="card-title">Laporan Per Barang {{$_GET['finaltgl']}}</h3>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-default btn-sm" id="printtable"><i
                                         class="fas fa-print"></i>
@@ -60,7 +60,8 @@
                                             <td>{{$i++}}</td>
                                             <td>{{$row->namaproduk}} ({{$row->namawarna}} - {{$row->namasize}})</td>
                                             <td>{{$row->jumlahtotal}} Pcs</td>
-                                            <td class="text-right">{{"Rp ". number_format($row->grandtotal,0,',','.')}}</td>
+                                            <td class="text-right">{{"Rp ". number_format($row->grandtotal,0,',','.')}}
+                                            </td>
                                         </tr>
                                         @endforeach
                                         <tr>
@@ -81,6 +82,41 @@
         </div>
     </div>
 </div>
+<div id="cetaktabel" style="display:none;">
+    <h5>Laporan Perbarang {{$_GET['finaltgl']}}</h5>
+    <table border="1" style="border-collapse: collapse;border: 1px solid black;" width="100%">
+        <thead>
+            <tr style="border: 1px solid black;">
+                <th style="border: 1px solid black;">No</th>
+                <th style="border: 1px solid black;">Barang</th>
+                <th style="border: 1px solid black;">Jumlah Terjual</th>
+                <th style="border: 1px solid black;" align="center">Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+            $i=1;
+            $total=0;
+            @endphp
+            @foreach($data as $row)
+            @php
+            $total += $row->grandtotal;
+            @endphp
+            <tr style="border: 1px solid black;">
+                <td style="border: 1px solid black;">{{$i++}}</td>
+                <td style="border: 1px solid black;">{{$row->namaproduk}} ({{$row->namawarna}} - {{$row->namasize}})</td>
+                <td style="border: 1px solid black;">{{$row->jumlahtotal}} Pcs</td>
+                <td style="border: 1px solid black;" align="right">{{"Rp ". number_format($row->grandtotal,0,',','.')}}</td>
+            </tr>
+            @endforeach
+            <tr style="border: 1px solid black;">
+                <td style="border: 1px solid black;" colspan="3"><b>Total</b></td>
+                <td style="border: 1px solid black;" align="right"><b>{{"Rp ". number_format($total,0,',','.')}}</b>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 @endsection
 
 @push('customjs')
@@ -93,7 +129,7 @@ $(document).ready(function() {
     $("#exportexcel").click(function() {
         $('#list-data').tableExport({
             format: 'xls',
-            filename: 'Laporan Transaksi',
+            filename: 'Laporan Per Barang',
         });
     });
     $("#printtable").click(function() {
