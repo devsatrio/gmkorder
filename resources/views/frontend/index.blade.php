b888d75268d7706bb71cea69a1cbce55ac0d0d5f
peternak kode <anakmbarep999@gmail.com>
2021-03-20 18:46:25 +0700

    update setting web

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="shortcut icon" type="image/jpg" href="{{asset('images/setting').'/'.CekNotif::namaWeb()->favicon}}"/>
     <title>@yield('title',CekNotif::namaWeb()->nama)</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('frontend.css')
